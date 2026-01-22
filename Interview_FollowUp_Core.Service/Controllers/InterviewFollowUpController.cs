3946f0d50a77b754ab19967f3f9ccd4a429bf80a
Pranavkumar <pranav.kumar.ext@siemens.com>
2026-01-23 00:22:00 +0530

    Dict & Hash

--- Interview_FollowUp_Core.Service/Controllers/InterviewFollowUpController.cs
+++ Interview_FollowUp_Core.Service/Controllers/InterviewFollowUpController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 
 namespace Interview_FollowUp_Core.Service.Controllers
 {
@@ -7,6 +8,36 @@ namespace Interview_FollowUp_Core.Service.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public InterviewFollowUpController()
+        {
+
+        }
+
+        public void WhatisDictionary()
+        {
+            // Dictionary is a collection of key-value pairs
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "One");
+            dictionary.Add(2, "Two");
+            dictionary.Add(3, "Three");
+            foreach (KeyValuePair<int, string> kvp in dictionary)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            }
+        }
+
+        public void WhatIsHashtable()
+        {
+            // Hashtable is a collection of key-value pairs
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(1, "One");
+            hashtable.Add(2, "Two");
+            hashtable.Add(3, "Three");
+            foreach (DictionaryEntry de in hashtable)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);
+            }
         }
     }
 }
--- Interview_FollowUp_Core.Service/Program.cs
+++ Interview_FollowUp_Core.Service/Program.cs
@@ -9,16 +9,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddSingleton<IGuidService, GuidService>();
 builder.Services.AddScoped<IGuidService, GuidService>();
 
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    // Enable Swagger middleware
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -26,5 +36,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseRouting();
 app.Run();
