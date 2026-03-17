bcd84af8e79e4d47a5036a8131db3a0650ef1e68
Pranavkumar Palanisamy <pranavkumar_palanisamy@epam.com>
2026-03-17 15:36:50 +0530

    DB context Changes

---
+++ Interview_FollowUp_Core.Service/DBContext/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Interview_FollowUp_Core.Service.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Interview> Interviews { get; set; }
+    }
+}
